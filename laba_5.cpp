fb123d43225068cf01674b993efdd3369628a604
SashaAndriesku <sashaandriesku01@gmail.com>
2025-05-12 08:40:01 +0300

    polymorphism

--- laba_5.cpp
+++ laba_5.cpp
@@ -1,12 +1,8 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
+#include <locale>
+#include <windows.h>
 using namespace std;
-
-class IInteractable {
-public:
-    virtual void interact() const = 0;
-    virtual ~IInteractable() = default;
-};
 
 class LivingBeing {
 protected:
@@ -15,11 +11,26 @@ protected:
 
 public:
     LivingBeing() : name("Unknown"), age(0) {}
-    LivingBeing(const string& n, int a) : name(n), age(a) {}
-    LivingBeing(const LivingBeing& other) = default;
-    LivingBeing(LivingBeing&& other) noexcept = default;
-    LivingBeing& operator=(const LivingBeing& other) = default;
-    LivingBeing& operator=(LivingBeing&& other) noexcept = default;
+    LivingBeing(string n, int a) : name(n), age(a) {}
+    LivingBeing(const LivingBeing& other) : name(other.name), age(other.age) {}
+    LivingBeing(LivingBeing&& other) noexcept : name(move(other.name)), age(other.age) {}
+
+    LivingBeing& operator=(const LivingBeing& other) {
+        if (this != &other) {
+            name = other.name;
+            age = other.age;
+        }
+        return *this;
+    }
+
+    LivingBeing& operator=(LivingBeing&& other) noexcept {
+        if (this != &other) {
+            name = move(other.name);
+            age = other.age;
+        }
+        return *this;
+    }
+
     virtual ~LivingBeing() {
         cout << "LivingBeing " << name << " has been deleted\n";
     }
@@ -28,46 +39,70 @@ public:
         cout << "Name: " << name << ", Age: " << age << " years\n";
     }
 
-    virtual void speak() const {
-        cout << name << " makes a sound.\n";
+    virtual void greet() const {
+        cout << name << " greets you!\n";
     }
 
-    virtual void move() const {
-        cout << name << " is moving.\n";
+    static void staticMessage() {
+        cout << "Static method from LivingBeing\n";
     }
+
+    void setName(const string& n) { name = n; }
+    int getAge() const { return age; }
 };
 
-class Animal : public LivingBeing, public IInteractable {
+class Speaker {
+public:
+    virtual void speak() const = 0;
+    virtual ~Speaker() = default;
+};
+
+class Animal : public LivingBeing, public Speaker {
 protected:
     string breed;
     static int animalCount;
 
 public:
     Animal() : LivingBeing(), breed("Unknown") { animalCount++; }
-    Animal(const string& n, const string& b, int a) : LivingBeing(n, a), breed(b) { animalCount++; }
-    Animal(const Animal& other) = default;
-    Animal(Animal&& other) noexcept = default;
-    Animal& operator=(const Animal& other) = default;
-    Animal& operator=(Animal&& other) noexcept = default;
+    Animal(string n, string b, int a) : LivingBeing(n, a), breed(b) { animalCount++; }
+    Animal(string n) : Animal(n, "Unknown", 0) {}
+
+    Animal(const Animal& other) : LivingBeing(other), breed(other.breed) {
+        cout << "Copy constructor called for Animal: " << name << endl;
+        animalCount++;
+    }
+
+    Animal(Animal&& other) noexcept : LivingBeing(move(other)), breed(move(other.breed)) {
+        cout << "Move constructor called for Animal: " << name << endl;
+    }
+
+    Animal& operator=(const Animal& other) {
+        if (this != &other) {
+            LivingBeing::operator=(other);
+            breed = other.breed;
+        }
+        return *this;
+    }
+
+    Animal& operator=(Animal&& other) noexcept {
+        if (this != &other) {
+            LivingBeing::operator=(move(other));
+            breed = move(other.breed);
+        }
+        return *this;
+    }
+
     virtual ~Animal() {
         cout << "Animal " << name << " has been deleted\n";
         animalCount--;
     }
 
-    void showInfo() const override {
+    virtual void showInfo() const override {
         cout << "Animal: " << name << ", Breed: " << breed << ", Age: " << age << " years\n";
     }
 
     void speak() const override {
-        cout << name << " makes an animal sound.\n";
-    }
-
-    void move() const override {
-        cout << name << " the animal moves.\n";
-    }
-
-    void interact() const override {
-        cout << name << " interacts with you.\n";
+        cout << name << " makes a sound!\n";
     }
 
     Animal& operator++() {
@@ -83,27 +118,151 @@ public:
         os << "Animal: " << a.name << ", Breed: " << a.breed << ", Age: " << a.age;
         return os;
     }
+
+    friend istream& operator>>(istream& is, Animal& a) {
+        cout << "Enter name: "; is >> a.name;
+        cout << "Enter breed: "; is >> a.breed;
+        cout << "Enter age: "; is >> a.age;
+        return is;
+    }
+
+    static int getAnimalCount() { return animalCount; }
 };
 
 int Animal::animalCount = 0;
 
+class Dog final : public Animal {
+private:
+    string favoriteToy;
+
+public:
+    Dog() : Animal(), favoriteToy("Ball") {}
+    Dog(string n, string b, int a, string toy) : Animal(n, b, a), favoriteToy(toy) {}
+
+    void showInfo() const override {
+        Animal::showInfo();
+        cout << "Favorite Toy: " << favoriteToy << endl;
+    }
+
+    void speak() const override {
+        cout << name << " barks!\n";
+    }
+};
+
+class Cat : public Animal {
+private:
+    bool isIndoor;
+
+public:
+    Cat() : Animal(), isIndoor(true) {}
+    Cat(string n, string b, int a, bool indoor) : Animal(n, b, a), isIndoor(indoor) {}
+
+    void showInfo() const override {
+        Animal::showInfo();
+        cout << "Indoor Cat: " << (isIndoor ? "Yes" : "No") << endl;
+    }
+
+    void speak() const override {
+        cout << name << " meows!\n";
+    }
+};
+
+class Person : public LivingBeing {
+protected:
+    string phoneNumber;
+
+public:
+    Person() : LivingBeing(), phoneNumber("Unknown") {}
+    Person(string n, int a, string p) : LivingBeing(n, a), phoneNumber(p) {}
+
+    virtual void showInfo() const override {
+        LivingBeing::showInfo();
+        cout << "Phone: " << phoneNumber << endl;
+    }
+
+    void greet() const override {
+        cout << name << " says hi!\n";
+    }
+};
+
+class Adopter : public Person {
+public:
+    Adopter() : Person() {}
+    Adopter(string fn, int a, string pn) : Person(fn, a, pn) {}
+    Adopter(string fn) : Person(fn, 18, "Unknown") {}
+
+    ~Adopter() override {
+        cout << "Adopter " << name << " has been deleted\n";
+    }
+};
+
+class Shelter {
+private:
+    string name;
+    string address;
+    int capacity;
+
+public:
+    Shelter() : name("Unknown"), address("Unknown"), capacity(0) {}
+    Shelter(string n, string a, int c) : name(n), address(a), capacity(c) {}
+    Shelter(string n) : Shelter(n, "Unknown", 10) {}
+
+    ~Shelter() {
+        cout << "Shelter " << name << " has been deleted\n";
+    }
+
+    void showInfo() const {
+        cout << "Shelter: " << name << ", Address: " << address << ", Capacity: " << capacity << endl;
+    }
+};
+
+void demonstrateStaticBinding(LivingBeing obj) {
+    obj.greet();
+}
+
+void demonstrateDynamicBindingByPointer(const LivingBeing* lb) {
+    lb->greet();
+}
+
+void demonstrateDynamicBindingByReference(const LivingBeing& lb) {
+    lb.greet();
+}
+
 int main() {
-    LivingBeing lb("Being", 5);
-    lb.showInfo();
-    lb.move();
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
 
-    Animal a1("Leo", "Cat", 3);
-    Animal a2("Max", "Dog", 5);
+    Dog dog("Barsik", "Labrador", 3, "Bone");
+    Cat cat("Murka", "Siamese", 5, true);
+    Animal mixed = dog + cat;
 
-    a1.showInfo();
-    a1.move();
-    a1.interact();
+    Adopter adopter("Oleksandr", 25, "+380123456789");
+    Shelter shelter("Good Friend", "Kyiv, Lisova St. 5", 50);
 
-    Animal a3 = a1 + a2;
-    cout << a3 << endl;
+    dog.showInfo();
+    cat.showInfo();
+    mixed.showInfo();
+    adopter.showInfo();
+    shelter.showInfo();
 
-    ++a1;
-    cout << a1 << endl;
+    cout << "Total animals: " << Animal::getAnimalCount() << endl;
+
+    ++dog;
+    dog.showInfo();
+
+    Animal newAnimal;
+    cin >> newAnimal;
+    cout << newAnimal << endl;
+
+    demonstrateStaticBinding(dog);
+    LivingBeing* ptr = &cat;
+    demonstrateDynamicBindingByPointer(ptr);
+    demonstrateDynamicBindingByReference(dog);
+
+    Speaker* s1 = &dog;
+    Speaker* s2 = &cat;
+    s1->speak();
+    s2->speak();
 
     return 0;
 }
